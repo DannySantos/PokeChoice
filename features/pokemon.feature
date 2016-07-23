58b256754c08ea1572c6a7d14252fa8c29289d67
Danny Santos <danny@ethicalswitch.com>
2016-07-23 17:54:24 +0100

    Initial commit

---
+++ features/pokemon.feature
@@ -0,0 +1,5 @@
+Feature: Pokemon
+
+  Scenario: A visitor views all Pokemon
+    Given they are on the home page
+    Then they should see all Pokemon in the database
---
+++ features/step_definitions/pokemon_steps.rb
@@ -0,0 +1,7 @@
+Given(/^they are on the home page$/) do
+  visit root_path
+end
+
+Then(/^they should see all Pokemon in the database$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
